77f1674b38298b184cf83f53bb70121e598c2e4a
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2018-12-14 14:15:32 +0200

    Introduce Tuttifrutti.Cache.Storage
    Tuttifrutti.Cache.Storage provides an interface intended for storing
    cached values with a basic set of operations to CRUD and expire the
    items.
    
    The provided implemnetation (Tuttifrutti.Cache.Storage.InMemory)
    maintains the cache in an in-memory HashPSQ. A postgres backend is
    coming soon.
    
    All of that is based on cache implementations in Faro and Persona
    and should cover the needs of both.

---
+++ src/Tuttifrutti/Cache/Storage.hs
@@ -0,0 +1,47 @@
+module Tuttifrutti.Cache.Storage where
+
+import           Tuttifrutti.Prelude
+
+import           Data.Range.Range    (Range (..))
+
+-- | Storage implementation. This defines primitive operations, from which
+--   all others are derived.
+data Handle k p v m = Handle
+  { -- ^ Insert, update, or delete element under a given key.
+    alter     :: forall a. (Maybe (p, v) -> (a, Maybe (p, v))) -> k -> m a
+    -- ^ Drops elements whose priority falls into a given range.
+  , dropRange :: Range p -> m ()
+  }
+
+-- | Allows to change the monad in which the storage actions are run.
+transHandle :: (forall a. m0 a -> m1 a) -> Handle k p v m0 -> Handle k p v m1
+transHandle nat Handle{..} = Handle
+  { dropRange = \p -> nat $ dropRange p
+  , alter = \f k -> nat $ alter f k
+  }
+
+-- | Insert a value.
+insert :: Handle k p v m -> (k, p, v) -> m ()
+insert h (k, p, v) = alter h (const ((), Just (p, v))) k
+
+-- | Delete a value under given key , returns the deleted value and its priority.
+delete :: Functor m => Handle k p v m -> k -> m (Maybe (p, v))
+delete h = alter h (,Nothing)
+
+-- | Lookup a value under a key. Takes a function that checks whether
+--   the element is expired. If that function returns 'Nothing' the item
+--   will be evicted.
+lookupValid
+  :: Handle k p v m
+  -> k
+  -> (p -> v -> Maybe a)
+  -> m (Maybe a)
+lookupValid h k validate =
+  alter h
+    (maybe (Nothing, Nothing)
+      (\pv@(uncurry validate -> a) -> (a, pv <$ a)))
+    k
+
+-- | Drop elements whose priority is less than given value.
+dropLowerThan :: Handle k p v m -> p -> m ()
+dropLowerThan h = dropRange h . UpperBoundRange
