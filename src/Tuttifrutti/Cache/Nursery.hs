09cf48c21854efe8366fab10f0cfb1e421ba6f92
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2018-12-18 13:07:25 +0200

    Add Nursery implementation
    The so-called "nursery" (analogous to GC terminology) is the place
    where the young cache entries live. When we fire of a request we add
    an entry to the nursery, so that other requesters can wait for it.
    When the request is done everybody who are waiting for it in the
    nursery would pick it up and then it will be moved to the cache storage.

---
+++ src/Tuttifrutti/Cache/Nursery.hs
@@ -0,0 +1,76 @@
+module Tuttifrutti.Cache.Nursery where
+
+import           Tuttifrutti.Prelude
+
+import           Data.HashPSQ              (HashPSQ)
+import qualified Data.HashPSQ              as HashPSQ
+import qualified Data.Time                 as Time
+import qualified Tuttifrutti.Time          as Time
+
+import qualified Tuttifrutti.Cache.Storage as Storage
+
+data Handle k p v = Handle
+  { nurseryQueue :: TVar (HashPSQ k UTCTime (Async (p, v))) }
+
+nursedIOStorage
+  :: (Hashable k, Ord k, Ord p)
+  => MonadIO m
+  => Handle k p v
+  -> Storage.Handle k p v m
+  -> Storage.Handle k p v m
+nursedIOStorage = nursedStorage atomically
+
+nursedSTMStorage
+  :: (Hashable k, Ord k, Ord p)
+  => Handle k p v
+  -> Storage.Handle k p v STM
+  -> Storage.Handle k p v STM
+nursedSTMStorage = nursedStorage id
+
+nursedStorage
+  :: (Hashable k, Ord k, Ord p)
+  => Monad m
+  => (forall a. STM a -> m a)
+  -> Handle k p v
+  -> Storage.Handle k p v m
+  -> Storage.Handle k p v m
+nursedStorage liftSTM Handle{..} storage = Storage.Handle
+  { alter = \f k -> do
+      -- wait/get the the value from nursery (if any)
+      nursed <- liftSTM $ traverse waitSTM =<< lookupAsync Handle{..} k
+      a <- Storage.alter storage
+        (\stored ->
+           f $ case (nursed, stored) of
+             (Nothing, Nothing) -> Nothing
+             (Just pv, Nothing) -> Just pv
+             (Nothing, Just pv) -> Just pv
+             (Just (nursedP, nursedV), Just (storedP, storedV))
+               | nursedP > storedP -> Just (nursedP, nursedV)
+               | otherwise -> Just (storedP, storedV)
+        )
+        k
+      liftSTM $ modifyTVar nurseryQueue $ HashPSQ.delete k
+      pure a
+  , dropRange = Storage.dropRange storage
+  }
+
+newHandle :: STM (Handle k p v)
+newHandle = do
+  nurseryQueue <- newTVar HashPSQ.empty
+  pure Handle{..}
+
+insertAsync
+  :: (Hashable k, Ord k)
+  => Handle k p v -> k -> UTCTime -> Async (p, v) -> STM ()
+insertAsync Handle{..} k now v = do
+  modifyTVar nurseryQueue $ HashPSQ.insert k now v . trim
+  where
+    -- remove entries that were sitting there since before the threshold
+    trim = snd . HashPSQ.atMostView threshold
+    threshold = Time.addUTCTime (negate 10 * Time.nominalMinute) now
+
+lookupAsync
+  :: (Hashable k, Ord k)
+  => Handle k p v -> k -> STM (Maybe (Async (p, v)))
+lookupAsync Handle{..} k =
+  fmap snd . HashPSQ.lookup k <$> readTVar nurseryQueue
