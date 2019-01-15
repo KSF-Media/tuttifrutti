5c4b7b77910cc9777979ff600cd0bab0546c5d64
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2019-01-15 15:29:07 +0200

    Add Tuttifrutti.Pool
    A wraper module around `Data.Pool` to fix some of its inconviniences.
    The main aim here is to make creating resource pools nice and easy
    so that other modules don't have to provide convinience functions
    for creating pools, they'll just provide functions to allocate/release
    their resources and the caller will have to create the pool with
    desired configuration.

---
+++ src/Tuttifrutti/Pool.hs
@@ -0,0 +1,39 @@
+module Tuttifrutti.Pool
+  ( module Tuttifrutti.Pool
+  , module Data.Pool
+  ) where
+
+import           Data.Time           (NominalDiffTime)
+import           Tuttifrutti.Prelude
+
+import           Data.Pool           hiding (createPool, withResource)
+import qualified Data.Pool           as Data.Pool
+
+data Config = Config
+  { -- ^ amount of stripes (distinct sub-pools) to maintain
+    configStripesAmount :: Int
+    -- ^ amount of time for which an unused resource is kept open
+  , configUnusedTimeout :: NominalDiffTime
+    -- ^ maximum number of resources to keep open per stripe
+  , configResourcesMax  :: Int
+  } deriving (Show, Eq, Ord)
+
+-- | Same as 'Data.Pool.createPool' but takes nicer 'Config' type.
+createPool
+  :: MonadIO m
+  => IO a         -- ^ action to create the resource
+  -> (a -> IO ()) -- ^ action to destroy the resource
+  -> Config
+  -> m (Pool a)
+createPool create destroy Config{..} =
+  liftIO $ Data.Pool.createPool
+    create destroy
+    configStripesAmount
+    configUnusedTimeout
+    configResourcesMax
+
+-- | Same as 'Data.Pool.withResource' but uses 'MonadUnliftIO'.
+withResource :: MonadUnliftIO m => Pool a -> (a -> m b) -> m b
+withResource pool action =
+  withRunInIO $ \runInIO -> Data.Pool.withResource pool $ runInIO . action
+
