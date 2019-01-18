7a134682e43c2d204a46603958fcd465b0e8be92
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2019-01-18 17:09:31 +0200

    Fixup doctests

---
+++ src/Tuttifrutti/Package.hs
@@ -0,0 +1,25 @@
+-- | Some meta utilities to access Haskell packaging data.
+module Tuttifrutti.Package where
+
+import           Prelude
+import           Tuttifrutti.Prelude
+
+import           Control.Lens        (mapped, (^..))
+
+import qualified Data.Aeson          as Json
+import qualified Data.Aeson.Lens     as Json
+import qualified Data.Yaml           as Yaml
+
+-- | Get extensions that are enabled by default in a file
+--   at a given path.
+--   FIXME: Support cabal files.
+getDefaultExtensions :: FilePath -> IO [Text]
+getDefaultExtensions path = do
+  packageInfo :: Json.Value <- Yaml.decodeFileThrow path
+  let extensions =
+        packageInfo
+          ^.. Json.key "default-extensions"
+            . Json.values
+            . Json._String
+  pure extensions
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,29 @@
 module Main where
 
+import qualified Data.Text               as Text
 import           Tuttifrutti.Prelude
 
+import           Test.DocTest            (doctest)
 import           Test.Tasty              as Tasty
 
 import qualified Tuttifrutti.Cache.Tests
+import qualified Tuttifrutti.Package     as Package
 
 main :: IO ()
-main = Tasty.defaultMain tests
+main = do
+  Tasty.defaultMain tests
+  doctests
+
+doctests :: IO ()
+doctests = do
+  languageExtensions <- Package.getDefaultExtensions "package.yaml"
+  let extensionFlags = Text.unpack . Text.append "-X" <$> languageExtensions
+      flags =
+        [ "-X" <> Text.unpack extension | extension <- languageExtensions ]
+          <>
+        ["--verbose"]
+      paths = ["src/"]
+  doctest (flags <> paths)
 
 tests :: Tasty.TestTree
 tests = Tasty.testGroup "Main"
