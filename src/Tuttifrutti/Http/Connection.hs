8112b4919ef26933b3762aca22c7b7830c7b694b
Konstantin Zudov <konstantin.zudov@ksfmedia.fi>
2019-05-07 12:29:39 +0300

    Add Tuttifrutti.Http.Connection with ConnectionProxy
    A helper for debugging http client weirdness, allows to setup every
    connection created by manager with callbacks that would e.g. log
    everything that goes through

---
+++ src/Tuttifrutti/Http/Connection.hs
@@ -0,0 +1,48 @@
+module Tuttifrutti.Http.Connection where
+
+import Tuttifrutti.Prelude
+
+import Network.HTTP.Client.Internal (Connection(..), Manager(..), ManagerSettings(..))
+
+data ConnectionProxy = ConnectionProxy
+  { connectionProxyRead  :: ByteString -> IO ByteString
+    -- ^ called whenever somebody reads from connection
+  , connectionProxyUnread :: ByteString -> IO ByteString
+    -- ^ called whenever somebody returns some read data
+  , connectionProxyWrite :: ByteString -> IO ByteString
+    -- ^ called whenever somebody writes to connection
+  , connectionProxyClose :: IO ()
+    -- ^ called to close the connection
+  }
+
+-- | Pass the given 'Connection' through a given 'ConnectionProxy'
+proxyConnection :: ConnectionProxy -> Connection -> Connection
+proxyConnection ConnectionProxy{..} Connection{..} =
+  Connection
+    { connectionRead = connectionProxyRead =<< connectionRead
+    , connectionWrite = \bs -> connectionWrite =<< connectionProxyWrite bs
+    , connectionUnread = \bs -> connectionUnread =<< connectionProxyUnread bs
+    , connectionClose = connectionProxyClose `finally` connectionClose
+    }
+
+proxyConnectionManagerSettings :: IO ConnectionProxy -> ManagerSettings -> ManagerSettings
+proxyConnectionManagerSettings makeConnectionProxy ManagerSettings{..} =
+  ManagerSettings
+    { managerRawConnection = do
+        makeRawConnection <- managerRawConnection
+        connectionProxy <- makeConnectionProxy
+        pure $ \addr host port ->
+          proxyConnection connectionProxy <$> makeRawConnection addr host port
+    , managerTlsConnection = do
+        makeTlsConnection <- managerTlsConnection
+        connectionProxy <- makeConnectionProxy
+        pure $ \addr host port ->
+          proxyConnection connectionProxy <$> makeTlsConnection addr host port
+    , managerTlsProxyConnection = do
+        makeTlsProxyConnection <- managerTlsProxyConnection
+        connectionProxy <- makeConnectionProxy
+        pure $ \connstr checkConn serverName ha host port ->
+          proxyConnection connectionProxy
+            <$> makeTlsProxyConnection connstr checkConn serverName ha host port
+    , ..
+    }
