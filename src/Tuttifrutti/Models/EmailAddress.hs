2a0cb9b911ec0369ca6d6f410a122efa16f93cbc
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2019-09-10 16:54:43 +0300

    Add email validation

--- src/Tuttifrutti/Models/EmailAddress.hs
+++ src/Tuttifrutti/Models/EmailAddress.hs
@@ -2,16 +2,37 @@ module Tuttifrutti.Models.EmailAddress where
 
 import           Tuttifrutti.Prelude
 
-import           Data.Swagger
-
+import qualified Data.Aeson          as JSON
+import qualified Data.Swagger        as Swagger
+import qualified Data.Text           as Text
+import           Text.Regex.PCRE     (Regex, makeRegex, matchTest)
 
 newtype EmailAddress = EmailAddress { unEmailAddress :: Text }
   deriving ( Show, Eq, Ord
            , Generic, Data, Typeable
            , FromHttpApiData, ToHttpApiData
-           , ToJSON, FromJSON
-           , IsString
+           , ToJSON, IsString
            )
 
-instance ToSchema EmailAddress where
-  declareNamedSchema = genericDeclareNamedSchemaNewtype defaultSchemaOptions declareSchema
+instance Swagger.ToParamSchema EmailAddress where
+  toParamSchema _ = Swagger.toParamSchema (Proxy :: Proxy Text)
+    & Swagger.pattern ?~ emailRegex
+
+instance Swagger.ToSchema EmailAddress where
+  declareNamedSchema = Swagger.genericDeclareNamedSchemaNewtype Swagger.defaultSchemaOptions Swagger.declareSchema
+
+instance FromJSON EmailAddress where
+  parseJSON = JSON.withText "EmailAddress" $ \email ->
+    if isEmailAddressValid $ EmailAddress email
+    then pure $ EmailAddress email
+    else fail $ show EmailAddressInvalid
+
+data EmailAddressErrors = EmailAddressInvalid
+  deriving (Show)
+
+isEmailAddressValid :: EmailAddress -> Bool
+isEmailAddressValid (EmailAddress email) = matchTest (makeRegex (Text.unpack emailRegex) :: Regex) $ Text.unpack email
+
+-- | From https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#Validation
+emailRegex :: Text
+emailRegex = "^[a-zA-Z0-9.!#$%&'*+\\\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
