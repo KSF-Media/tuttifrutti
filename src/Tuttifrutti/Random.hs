5ade75fa250deba2f63b0e768c75a541fda6dfad
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2019-11-07 15:22:30 +0200

    Add randomNum (#58)

--- src/Tuttifrutti/Random.hs
+++ src/Tuttifrutti/Random.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedLists #-}
 module Tuttifrutti.Random where
 
 import           Tuttifrutti.Prelude
@@ -12,6 +13,11 @@ randomAlphaNum size = do
   pure $ Text.pack $ take size $ randomStreamOf alphaNum gen
   where
     alphaNum = Set.fromList (['a' .. 'z'] ++ ['A'..'Z'] ++ ['0'..'9'])
+
+randomNum :: MonadIO m => Int -> m Text
+randomNum size = do
+  gen <- liftIO System.Random.newStdGen
+  pure $ Text.pack $ take size $ randomStreamOf ['0'..'9'] gen
 
 -- | Given a set of values generates a random infinite stream with set members.
 randomStreamOf :: RandomGen g => Set a -> g -> [a]
