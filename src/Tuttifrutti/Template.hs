ea2b048f34a9b3f0e70048e4b85336073938a177
amorcian <morciano.andrea92@gmail.com>
2020-02-18 15:23:40 +0200

    Add Template module

--- src/Tuttifrutti/Template.hs
+++ src/Tuttifrutti/Template.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+<<<<<<< HEAD
 module Tuttifrutti.Template
   (render
   ) where
@@ -14,11 +15,31 @@ import qualified Data.Text.Template as Template
 
 -- | Create 'Context' from association list.
 context :: [(T.Text, T.Text)] -> Template.Context
+=======
+module Tuttifrutti.Template where
+
+import Prelude
+
+import qualified Data.ByteString.Lazy as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy.Encoding as E
+import           Data.Maybe
+
+import Data.Text.Template
+
+-- | Create 'Context' from association list.
+context :: [(T.Text, T.Text)] -> Context
+>>>>>>> Add Template module
 context assocs x = fromMaybe err . lookup x $ assocs
   where
     err = error $ "Could not find key: " ++ T.unpack x
 
+<<<<<<< HEAD
 render :: T.Text -> [(T.Text, T.Text)] -> T.Text
 render template' context' = S.toStrict $ Template.substitute template' context''
+=======
+render :: T.Text -> [(T.Text, T.Text)] -> S.ByteString
+render template' context' = E.encodeUtf8 $ substitute template' context''
+>>>>>>> Add Template module
   where
     context''  = context context'
