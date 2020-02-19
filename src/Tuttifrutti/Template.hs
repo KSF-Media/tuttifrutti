a5af8b686cbdac68021b22bed22a171f3f1b3e38
amorcian <morciano.andrea92@gmail.com>
2020-02-19 14:47:29 +0200

    Refactor Template module interface (#65)

--- src/Tuttifrutti/Template.hs
+++ src/Tuttifrutti/Template.hs
@@ -1,22 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Tuttifrutti.Template where
+module Tuttifrutti.Template
+  (render
+  ) where
 
 import Prelude
 
-import qualified Data.Text.Lazy as S
-import qualified Data.Text as T
+import qualified Data.Text.Lazy     as S
+import qualified Data.Text          as T
 import           Data.Maybe
 
-import Data.Text.Template
+import qualified Data.Text.Template as Template
 
 -- | Create 'Context' from association list.
-context :: [(T.Text, T.Text)] -> Context
+context :: [(T.Text, T.Text)] -> Template.Context
 context assocs x = fromMaybe err . lookup x $ assocs
   where
     err = error $ "Could not find key: " ++ T.unpack x
 
 render :: T.Text -> [(T.Text, T.Text)] -> T.Text
-render template' context' = S.toStrict $ substitute template' context''
+render template' context' = S.toStrict $ Template.substitute template' context''
   where
     context''  = context context'
