b089cb91425c03a2740998f0e1201d67f43a9ca9
amorcian <morciano.andrea92@gmail.com>
2020-02-19 14:40:05 +0200

    Fix types in Template module (#65)

--- src/Tuttifrutti/Template.hs
+++ src/Tuttifrutti/Template.hs
@@ -4,9 +4,8 @@ module Tuttifrutti.Template where
 
 import Prelude
 
-import qualified Data.ByteString.Lazy as S
+import qualified Data.Text.Lazy as S
 import qualified Data.Text as T
-import qualified Data.Text.Lazy.Encoding as E
 import           Data.Maybe
 
 import Data.Text.Template
@@ -17,7 +16,7 @@ context assocs x = fromMaybe err . lookup x $ assocs
   where
     err = error $ "Could not find key: " ++ T.unpack x
 
-render :: T.Text -> [(T.Text, T.Text)] -> S.ByteString
-render template' context' = E.encodeUtf8 $ substitute template' context''
+render :: T.Text -> [(T.Text, T.Text)] -> T.Text
+render template' context' = S.toStrict $ substitute template' context''
   where
     context''  = context context'
