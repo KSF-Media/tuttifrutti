8a9d2f52fd3737cc3bcc6e50d0341553bb905ba6
Andrea Morciano <morciano.andrea92@gmail.com>
2021-03-12 15:45:54 +0200

    Add PaperCode type (#79)
    * Add Paper type
    
    * Refactor Tuttifrutti.Models.Paper
    
    * Rename Paper to PaperCode
    
    * Refactor Tuttifrutti
    
    * Rename Tuttifrutti.Models.Paper to Tuttifrutti.Models.PaperCode

---
+++ src/Tuttifrutti/Models/PaperCode.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Tuttifrutti.Models.PaperCode where
+
+import           Tuttifrutti.Prelude
+
+import qualified Data.Text           as Text
+import           Database.Persist.TH (derivePersistField)
+
+data PaperCode
+  = HBL
+  | ON
+  | VN
+  | HT
+  deriving (Show, Eq, Generic, Read, Data, Ord)
+derivePersistField "PaperCode"
+
+instance FromJSON PaperCode
+instance ToJSON PaperCode
+
+toPaperCode :: Text -> PaperCode
+toPaperCode paperCodeText =
+  case Text.toUpper paperCodeText of
+    "HBL" -> HBL
+    "ON"  -> ON
+    "ÖN"  -> ON
+    "ÖNY" -> ON
+    "VN"  -> VN
+    "HT"  -> HT
+    _     -> HBL
