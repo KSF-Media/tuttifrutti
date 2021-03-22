c07ea2ba132da470038d601f3c3038147f3e7502
Andrea Morciano <morciano.andrea92@gmail.com>
2021-03-22 14:40:12 +0200

    Tag unknown paper codes and add custom paper code encode method (#81)
    * Fix Models.PaperCode
    
    * Override FromJSON, ToJSON instances
    * Fix Unjson instance to tag unrecognized paper codes and account
      for alternate spellings
    
    * Refactor Models.PaperCode
    
    * Factor fromPaperCode out of toJSON

--- src/Tuttifrutti/Models/PaperCode.hs
+++ src/Tuttifrutti/Models/PaperCode.hs
@@ -3,8 +3,9 @@ module Tuttifrutti.Models.PaperCode where
 
 import           Tuttifrutti.Prelude
 
+import           Data.Aeson          (Value (String), withText)
 import qualified Data.Text           as Text
-import           Data.Unjson
+import           Data.Unjson         (Unjson (..), unjsonAeson)
 import           Database.Persist.TH (derivePersistField)
 
 data PaperCode
@@ -12,11 +13,14 @@ data PaperCode
   | ON
   | VN
   | HT
+  | UnknownPaperCode Text
   deriving (Show, Eq, Generic, Read, Data, Ord)
 derivePersistField "PaperCode"
 
-instance FromJSON PaperCode
-instance ToJSON PaperCode
+instance FromJSON PaperCode where
+  parseJSON = withText "PaperCode" (pure . toPaperCode)
+instance ToJSON PaperCode where
+  toJSON = String . fromPaperCode
 instance Unjson PaperCode where
   unjsonDef = unjsonAeson
 
@@ -29,4 +33,10 @@ toPaperCode paperCodeText =
     "ÖNY" -> ON
     "VN"  -> VN
     "HT"  -> HT
-    _     -> HBL
+    p     -> UnknownPaperCode p
+
+fromPaperCode :: PaperCode -> Text
+fromPaperCode paperCode =
+  case paperCode of
+    UnknownPaperCode p -> p
+    _                  -> tshow paperCode
