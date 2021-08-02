95a7ee67170b0e38bb76dc5c1ed77c3e85023365
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-08-02 09:51:00 +0300

    Add Config type

--- src/Tuttifrutti/Sentry.hs
+++ src/Tuttifrutti/Sentry.hs
@@ -2,7 +2,7 @@ module Tuttifrutti.Sentry where
 
 import           Tuttifrutti.Prelude
 
-import           Data.Aeson             (Value)
+import           Data.Aeson             (Value, withObject, (.:))
 import           Data.Aeson.Types       (Pair)
 
 import qualified Data.Text              as Text
@@ -11,6 +11,17 @@ import           System.Log.Raven.Types (SentryLevel (..), SentryRecord)
 import qualified Tuttifrutti.Log.Handle as Log
 
 newtype Handle = Handle { logSentry :: SentryLevel -> String -> (SentryRecord -> SentryRecord) -> IO () }
+
+data Config = Config
+  { configEnvironment :: String
+  , configDsn         :: String
+  }
+
+instance FromJSON Config where
+  parseJSON = withObject "SentryConfig" $ \v ->
+    Config
+      <$> v .: "environment"
+      <*> v .: "dsn"
 
 toSentryLevel :: Log.LogSeverity -> SentryLevel
 toSentryLevel = \case
