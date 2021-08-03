ceabc337899b570f14a3dc5015989a381cda02e4
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-08-03 14:00:48 +0300

    Make Sentry.Handle nicer (#92)

--- src/Tuttifrutti/Sentry.hs
+++ src/Tuttifrutti/Sentry.hs
@@ -2,15 +2,17 @@ module Tuttifrutti.Sentry where
 
 import           Tuttifrutti.Prelude
 
-import           Data.Aeson             (Value, withObject, (.:))
-import           Data.Aeson.Types       (Pair)
+import           Data.Aeson                             (Value, withObject,
+                                                         (.:))
 
-import qualified Data.Text              as Text
-import           System.Log.Raven.Types (SentryLevel (..), SentryRecord)
+import qualified Data.Text                              as Text
+import qualified System.Log.Raven                       as Sentry
+import qualified System.Log.Raven.Transport.HttpConduit as Sentry
+import           System.Log.Raven.Types                 (SentryLevel (..))
 
-import qualified Tuttifrutti.Log.Handle as Log
+import qualified Tuttifrutti.Log.Handle                 as Log
 
-newtype Handle = Handle { logSentry :: SentryLevel -> String -> (SentryRecord -> SentryRecord) -> IO () }
+newtype Handle = Handle { logSentry :: SentryLevel -> Text -> [(Text, Value)]  -> IO () }
 
 data Config = Config
   { configEnvironment :: String
@@ -23,12 +25,24 @@ instance FromJSON Config where
       <$> v .: "environment"
       <*> v .: "dsn"
 
+initSentry
+  :: String
+  -> [(String, String)]
+  -> String
+  -> SentryLevel
+  -> Text
+  -> [(Text, Value)]
+  -> IO ()
+initSentry dsn initialTags serviceName level logMessage extraTags = do
+  sentry <- Sentry.initRaven dsn (Sentry.tags initialTags) Sentry.sendRecord Sentry.stderrFallback
+  Sentry.register sentry serviceName level (Text.unpack logMessage) (Sentry.extra $ map (first Text.unpack) extraTags)
+
+disabledHandle :: SentryLevel -> Text -> [(Text, Value)] -> IO ()
+disabledHandle _ _ _ = pure ()
+
 toSentryLevel :: Log.LogSeverity -> SentryLevel
 toSentryLevel = \case
   Log.LogTrace   -> Debug
   Log.LogInfo    -> Info
   Log.LogWarning -> Warning
   Log.LogError   -> Error
-
-toSentryParams :: [Pair] -> [(String, Value)]
-toSentryParams = map (first Text.unpack)
