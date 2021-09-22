d847f7c86a817cc6eeb46a3bacc1604804084c24
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2021-09-22 17:18:56 +0300

    Add missing instances for PaperCode (#97)

--- src/Tuttifrutti/Models/PaperCode.hs
+++ src/Tuttifrutti/Models/PaperCode.hs
@@ -3,12 +3,18 @@ module Tuttifrutti.Models.PaperCode where
 
 import           Tuttifrutti.Prelude
 
-import           Data.Aeson             (Value (String), withText)
-import           Data.Swagger           (ToSchema, declareNamedSchema)
-import qualified Data.Text              as Text
-import           Data.Text.Encoding     (decodeUtf8)
-import           Data.Unjson            (Unjson (..), unjsonAeson)
-import           Database.Persist.Types (fromPersistValueText)
+import           Data.Aeson                        (Value (String), withText)
+import           Data.Swagger                      (SwaggerType (..), ToSchema,
+                                                    declareNamedSchema, enum_,
+                                                    type_)
+import           Data.Swagger.Internal.ParamSchema (ToParamSchema,
+                                                    toParamSchema)
+import qualified Data.Text                         as Text
+import           Data.Text.Encoding                (decodeUtf8)
+import           Data.Unjson                       (Unjson (..), unjsonAeson)
+import           Database.Persist.Types            (fromPersistValueText)
+import           Web.HttpApiData                   (FromHttpApiData (..))
+
 
 data PaperCode
   = HBL
@@ -47,6 +53,14 @@ instance PersistField PaperCode where
 
 instance ToSchema PaperCode where
   declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy Text)
+
+instance ToParamSchema PaperCode where
+  toParamSchema _ = mempty
+     & type_ ?~ SwaggerString
+     & enum_ ?~ (map toJSON [ HBL, ON, VN, HT, JUNIOR, FORUM, LS ])
+
+instance FromHttpApiData PaperCode where
+  parseQueryParam = Right . toPaperCode
 
 toPaperCode :: Text -> PaperCode
 toPaperCode paperCodeText =
